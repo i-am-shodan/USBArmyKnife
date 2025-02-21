01e812b80ff57a4d54048d1b64c788aebd02181e
Besi <92743858+Besix2@users.noreply.github.com>
2025-02-21 16:30:36 +0100

    Update craft_agent.sh

--- tools/craft_agent.sh
+++ tools/craft_agent.sh
@@ -1,3 +1,21 @@
+#Download gh cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+        && sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+        && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && sudo apt update \
+        && sudo apt install gh -y
+
+#Clone Repository
+git clone https://github.com/i-am-shodan/USBArmyKnife.git
+
+#Donwload gent binaries
+cd USBArmyKnife
+gh auth login
+gh run download
+
 # Find a free loop device
 LOOP_DEVICE=$(losetup -f)
 
@@ -16,7 +34,7 @@ mkfs.vfat "${LOOP_DEVICE}p1"
 # Mount and copy files
 mkdir -p /tmp/mnt
 mount "${LOOP_DEVICE}p1" /tmp/mnt
-cp agent/* /tmp/mnt/
+cp PortableApp.dll turbojpeg.dll vcruntime140.dll in1.bat /tmp/mnt/
 
 # Cleanup
 umount /tmp/mnt
