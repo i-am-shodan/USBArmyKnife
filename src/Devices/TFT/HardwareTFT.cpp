ab220ca615a42c962c423abee6fe978e90a664f3
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2025-04-01 17:44:56 +0100

    what works:
    * screen
    * file system
    * mic
    * HID
    * WiFi

--- src/Devices/TFT/HardwareTFT.cpp
+++ src/Devices/TFT/HardwareTFT.cpp
@@ -26,6 +26,8 @@ class LGFX_Panel : public lgfx::LGFX_Device
     lgfx::Panel_ST7789 _panel_instance;
 #elif defined(WAVESHARE_ESP32_S3_GEEK)
     lgfx::Panel_ST7789 _panel_instance;
+#elif defined(LILYGO_T_WATCH_S3)
+    lgfx::Panel_ST7789 _panel_instance;
 #else
     #error Invalid display type // Full list here: https://github.com/lovyan03/LovyanGFX/tree/master/src/lgfx/v1/panel
 #endif
@@ -91,6 +93,12 @@ public:
             cfg.offset_y = 40;                 // Y direction offset amount of the panel
             cfg.dummy_read_pixel = 8;         // Number of bits for dummy read before pixel read
             cfg.dummy_read_bits = 1;          // Number of dummy read bits before non-pixel data read  
+#elif defined(LILYGO_T_WATCH_S3)
+            cfg.offset_rotation = 2;
+            cfg.offset_x = 0;                // Panel offset in X direction
+            cfg.offset_y = 0;                 // Y direction offset amount of the panel
+            cfg.dummy_read_pixel = 8;         // Number of bits for dummy read before pixel read
+            cfg.dummy_read_bits = 1;          // Number of dummy read bits before non-pixel data read
 #else
     #error Invalid display type // Full list here: https://github.com/lovyan03/LovyanGFX/tree/master/src/lgfx/v1/panel
 #endif
